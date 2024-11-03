cab924f5e2697c6b6368804297976543bc0a299a
gabri32 <88991735+gabri32@users.noreply.github.com>
2024-11-03 14:34:44 -0500

    fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Home from './components/Home';
-import Homees from './components/HomeSP'
 
 const App: React.FC = () => {
   return (
     <div>
-      <Home />,
-      <Homees/>
+      <Home />
       
     </div>
   );
